56c023c80b215df05dcd15f3cd521d96fd6d998f
rezamuhammad13 <138437186+rezamuhammad13@users.noreply.github.com>
2023-07-07 20:06:45 +0700

    admin panel

---
+++ admin panel/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Admin Panel</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="app/plugins/fontawesome-free/css/all.min.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="app/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="app/dist/css/adminlte.min.css">
+</head>
+
+<body class="hold-transition login-page">
+  <div class="login-box">
+    <!-- /.login-logo -->
+    <div class="card card-outline card-primary">
+      <div class="card-header text-center">
+        <a href="#" class="h1"><b>Admin</b>Hotel</a>
+      </div>
+      <div class="card-body">
+        <p class="login-box-msg">Log in to dashboard page </p>
+
+        <form action="app/model/login.php" method="post">
+          <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Username" name="username" required>
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-user"></span>
+              </div>
+            </div>
+          </div>
+          <div class="input-group mb-4">
+            <input type="password" class="form-control" placeholder="Password" name="password" required>
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-lock"></span>
+              </div>
+            </div>
+          </div>
+          <div class="row ">
+            <div class="col-sm-6 mx-auto">
+              <button type="submit" class="btn btn-primary btn-block">Log In</button>
+            </div>
+            <!-- /.col -->
+          </div>
+        </form>
+
+      </div>
+      <!-- /.card-body -->
+    </div>
+    <!-- /.card -->
+    <?php
+
+    session_start();
+
+    if(isset($_SESSION['login_gagal'])){
+      echo $_SESSION['login_gagal'];
+    }
+
+    $pageWasRefreshed = isset($_SERVER['HTTP_CACHE_CONTROL']) && $_SERVER['HTTP_CACHE_CONTROL'] === 'max-age=0';
+
+    if ($pageWasRefreshed) {
+      if (isset($_SESSION['login_gagal'])) {
+          unset($_SESSION['login_gagal']);
+      }
+  }
+
+?>
+  </div>
+  <!-- /.login-box -->
+
+  <!-- jQuery -->
+  <script src="plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="dist/js/adminlte.min.js"></script>
+</body>
+
+</html>
